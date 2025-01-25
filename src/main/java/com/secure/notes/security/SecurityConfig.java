c11b7220be2e30bee99c10fbfe13aac983da29f5
Bank <blankinfear@gmail.com>
2025-01-26 03:42:51 +0700

    learn about deny

--- src/main/java/com/secure/notes/security/SecurityConfig.java
+++ src/main/java/com/secure/notes/security/SecurityConfig.java
@@ -17,6 +17,8 @@ public class SecurityConfig {
         http.authorizeHttpRequests((requests) -> requests
                 .requestMatchers("/contact").permitAll()
                 .requestMatchers("/public/**").permitAll()
+                .requestMatchers("/admin").denyAll()
+                .requestMatchers("/admin/**").denyAll()
                 .anyRequest().authenticated());
         //http.formLogin(withDefaults());
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));

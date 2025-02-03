ce7929b0d0ac0b1490b07d65368d7d93d4af17bf
Bank <blankinfear@gmail.com>
2025-02-04 03:29:41 +0700

    add hash password

--- src/main/java/com/secure/notes/controllers/AdminController.java
+++ src/main/java/com/secure/notes/controllers/AdminController.java
@@ -13,13 +13,13 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin")
-//@PreAuthorize("hasRole('ROLE_ADMIN')")
+@PreAuthorize("hasRole('ROLE_ADMIN')")
 public class AdminController {
 
     @Autowired
     UserService userService;
 
-    //@PreAuthorize("hasRole('ROLE_ADMIN')")
+
     @GetMapping("/getusers")
     public ResponseEntity<List<User>> getAllUsers() {
         return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
@@ -32,7 +32,7 @@ public class AdminController {
         return ResponseEntity.ok("User role updated");
     }
 
-    //@PreAuthorize("hasRole('ROLE_ADMIN')")
+
     @GetMapping("/user/{id}")
     public ResponseEntity<UserDTO> getUser(@PathVariable Long id) {
         return new ResponseEntity<>(userService.getUserById(id), HttpStatus.OK);
